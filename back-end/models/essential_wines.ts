bc0bce5e099014fbf132a157a873ce3fb308a977
radhakrishna7362 <7krishna3radha@gmail.com>
2020-10-31 16:13:21 +0530

    Minor change

---
+++ back-end/models/essential_wines.ts
@@ -0,0 +1,57 @@
+export const essential_wines=[
+    {
+        "id": 200,
+        "name": "Essential Dinner Pairings - Multipack",
+        "description": "Go-tos for easy weeknights.",
+        "price": "$34.99",
+        "image": "https://media.blueapron.com/market/app/public/spree/products/2157/large/20_0914_FallWinterWineALC_Essential-Dinner-Pairings_Multi_Wine_Winter.jpg",
+        "summary":[
+            {"msg":"Whether you’re in the mood for eating something light, heavy, or in-between, this three-bottle bundle covers all your bases. It includes versatile reds from popular pairing keys—light and bright, fruity and savory, bold and spicy—which makes it perfect for pairing with a variety of foods."},
+            {"msg":"The 2017 Lusu Cellars Carignan is a medium-bodied wine with notes of herbs and red berries that make it incredibly versatile and perfect for pairing with everything from red meat to Mexican food. Rated 91 by wine critic James Suckling, the 2015 Château Pilet Bordeaux is a rich, full-bodied wine that balances notes of dark fruits and spices with bright acidity, and pairs well with meats such as lamb. Lastly, the 2016 Le P'tit Paysan '2.0' Cabernet Sauvignon is a pert, Old World-style cab with notes of black currant, earth, and dried herbs."}
+        ]
+    },
+    {
+        "id": 201,
+        "name": "Essential Dinner Pairings - Poultry",
+        "description": "Roasting a bird? These wines will pair perfectly.",
+        "price": "$34.99",
+        "image": "https://media.blueapron.com/market/app/public/spree/products/2161/large/20_0914_FallWinterWineALC_Essential-Dinner-Pairings_Poultry_Wine_Winter.jpg",
+        "summary":[
+            {"msg":"For enjoying meals with chicken or turkey, this three-bottle bundle features two reds and one white that perfectly complement poultry with their modern, refreshing flavors. All three wines hail from California, delivering a diverse set of characteristics from both warm and cool climates."},
+            {"msg":"The herbal notes and tannins of the 2017 Brick & Mortar Cabernet Franc make this a full-bodied wine that complements flavors in chicken or turkey dishes, while Lusu Cellars’ 'The Farmer's Hand' Red Blend is a fresh spin on the bolder reds you find in Australia and Spain. The herbal and savory notes make it a perfect pairing for roasted veggies. Lastly, the 2016 Mount Konocti Sauvignon Blanc is our ideal mix of tropical, grassy, and citrus notes that make a lively companion to poultry."}
+        ]
+    },
+    {
+        "id": 202,
+        "name": "Essential Dinner Pairings - Seafood",
+        "description": "Liven up seafood with these punchy wines.",
+        "price": "$34.99",
+        "image": "https://media.blueapron.com/market/app/public/spree/products/2159/large/20_0914_FallWinterWineALC_Essential-Dinner-Pairings_Seafood_Wine_Winter.jpg",
+        "summary": [
+            {"msg":"If you love pairing wine with seafood, this three-bottle bundle has just what you need. Featuring one red and two whites, this versatile assortment features crisp and zesty flavors that liven up lighter fare such as fish or seafood."},
+            {"msg":"The 2017 Lusu Cellars Carignan is a medium-bodied red wine with notes of herbs and red berries that make it easy to pair with a wide range of foods. The 2016 Bellbird White Blend is a zesty mix of Chardonnay and French Colombard, a grape normally distilled for Cognac, while the 2017 Brick & Mortar Chenin Blanc has the crispness of classic French chenin blanc paired with fruity notes that are characteristic of California."}
+        ]
+    },
+    {
+        "id": 203,
+        "name": "Essential Dinner Pairings - Steak",
+        "description": "Reds that can stand up to your steak.",
+        "price": "$34.99",
+        "image": "https://media.blueapron.com/market/app/public/spree/products/2167/large/20_0914_FallWinterWineALC_Essential-Cab-Sauvs_Wine_Winter.jpg",
+        "summary": [
+            {"msg":"Ideal for any steak lover (and anyone who loves a good cabernet sauvignon), this three-bottle bundle includes three of our favorite cabs for comparing tasting notes as well as pairing with hearty steak dinners. While two of the wines hail from California’s central coast, one is a New World-style cab sauv crafted by a Washington winery operating out of two former Army Air Corps airplane hangars."},
+            {"msg":"Each of these full-bodied wines brings robust, spicy flavors to the table, perfect for enjoying with the decadent taste of steak and red meat. The 2016 Airfield Estates Cabernet Sauv balances plush fruit flavors with spice and savory notes, offering acidic tanginess. The 2016 Le P'tit Paysan Cabernet Sauvignon is a lively style that leads with notes of pepper and oregano, while the 2017 Cabernet Sauvignon from the same winery is a true expression of this varietal, an Old World-style cab with notes of black currant and dried herbs."}
+        ]
+    },
+    {
+        "id": 204,
+        "name": "Essential Dinner Pairings - Stir Fry",
+        "description": "Refreshing wines to complement your savory takeout.",
+        "price": "$34.99",
+        "image": "https://media.blueapron.com/market/app/public/spree/products/2170/large/20_0914_FallWinterWineALC_Essential-Dinner-Pairings_Stirfry_Wine_Winter.jpg",
+        "summary":[
+            {"msg":"This must-try bundle features wines with Crisp & Minerally and Plush & Fruity pairing keys, making them ideal companions for an easy weeknight stir-fry or carry-out. Featuring refreshing and fruity notes with a bit of acidity, these wines cut through and complement the umami-rich flavors of Asian-style noodles, indulgent sauces, and meat."},
+            {"msg":"Like a Chardonnay from France’s Chablis region (versus a buttery California chardonnay), the 2016 Le P'tit Paysan Chardonnay has crisp and fruity notes with minerality. The 2017 Brick & Mortar Chenin Blanc has the crispness of classic French chenin blanc paired with fruity notes that are characteristic of California. The one red of the bundle, a 2017 Anthesis Pinot Noir, is a classic pinot with up-front fruit flavors accented by savory notes."}
+        ]
+    }
+]
