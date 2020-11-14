6942b05f6c6f94b6ae628e9168e01b0cdfc13d4d
radhakrishna7362 <7krishna3radha@gmail.com>
2020-11-15 01:11:39 +0530

    Minor change

--- mealmate/src/app/services/kitchentools.service.ts
+++ mealmate/src/app/services/kitchentools.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +9,17 @@ export class KitchentoolsService {
   constructor(private http:HttpClient) { }
 
   getKitchenTools(){
-    return this.http.get("http://localhost:3000/kitchen/menu");
+    return this.http.get<any>("http://localhost:3000/kitchen/menu",{
+      params:new HttpParams().append('token',localStorage.getItem('token'))
+    });
   }
 
   getMainKitchenTools(){
-    return this.http.get("http://localhost:3000/kitchen/home");
+    return this.http.get<any>("http://localhost:3000/kitchen/home");
   }
 
   getAKitchenTool(id){
-    return this.http.get(`http://localhost:3000/kitchen/particular/${id}`);
+    return this.http.get<any>(`http://localhost:3000/kitchen/particular/${id}`);
   }
   
 }
