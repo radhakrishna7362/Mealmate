a404fe0b5bc27b312e671e269f60ac27ad554206
radhakrishna7362 <7krishna3radha@gmail.com>
2020-11-15 01:09:05 +0530

    Singup page for Registration

---
+++ mealmate/src/app/register/register.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerUserData = { username: "", email: "", password: "" }
+  constructor(private _auth: AuthService,
+              private _router: Router,private snackbar:MatSnackBar) { }
+
+  ngOnInit() {
+  }
+
+  registerUser() {
+    this._auth.registerUser(this.registerUserData)
+    .subscribe(
+      res => {
+        localStorage.setItem('token', res.token)
+        this._router.navigate(['/home'])
+      },
+      err => {
+        if( err instanceof HttpErrorResponse ) {
+          if (err.status === 409) {
+            this.snackbar.open('EMAIL ALREADY REGISTERED', 'OK', {
+              duration: 3000,
+            });
+            this.registerUserData.username="";
+            this.registerUserData.email="";
+            this.registerUserData.password="";
+          }
+        }
+      }
+    )      
+  }
+}
