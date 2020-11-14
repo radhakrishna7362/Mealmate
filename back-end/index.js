96ae561d31c11bcb51cd3d15b03b4e5f0c86a40f
radhakrishna7362 <7krishna3radha@gmail.com>
2020-11-15 01:09:34 +0530

    Added userRoute to server

--- back-end/index.js
+++ back-end/index.js
@@ -2,30 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-mongoose.connect('mongodb+srv://rk:190031187@first.cpozf.mongodb.net/mealmate?retryWrites=true&w=majoritys',{useNewUrlParser: true , useUnifiedTopology: true});
-
 const connection = mongoose.connection;
-
-connection.once('open', () => console.log("connected to the mongodb"));
-
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
 const port = process.env.port || 3000;
 
+mongoose.connect('mongodb+srv://rk:190031187@first.cpozf.mongodb.net/mealmate?retryWrites=true&w=majority',{useNewUrlParser: true , useUnifiedTopology: true});
+
+connection.once('open', () => console.log("connected to the mongodb"));
 
 const FoodRoute=require('./routes/detailsRoute');
 const KitchenRoute=require('./routes/kitchentoolsRoute');
 const FoodDonation=require('./routes/food_donationRoute');
 const FallWineBundle=require('./routes/fall_wine_bundleRoute');
 const EssentialWine=require('./routes/essential_wineRoute');
+const UserRoute=require('./routes/userRoute');
+const CartRoute=require('./routes/cartRoute');
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/food',FoodRoute);
 app.use('/kitchen',KitchenRoute);
 app.use('/fooddonation',FoodDonation);
 app.use('/fallwinebundle',FallWineBundle);
 app.use('/essentialwine',EssentialWine);
+app.use('/user',UserRoute);
+app.use('/cart',CartRoute);
 
 app.listen(port, () => console.log(`running on the server ${port}`));
