b902ee24223dec8808643cc8b2f914d0e1699d9f
radhakrishna7362 <7krishna3radha@gmail.com>
2020-11-15 01:13:45 +0530

    Minor changes in UI

--- mealmate/src/app/fooddetails/fooddetails.component.ts
+++ mealmate/src/app/fooddetails/fooddetails.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { FoodService } from '../services/food.service';
+import { AuthService } from '../services/auth.service';
+import { CartService } from '../services/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
 // import { food } from '../services/food';
 
 @Component({
@@ -11,7 +15,9 @@ import { FoodService } from '../services/food.service';
 export class FooddetailsComponent implements OnInit {
 
   food;
-  constructor(private route:ActivatedRoute,private foodService:FoodService) { }
+  cart={userid:null,productid:null};
+  constructor(private route:ActivatedRoute,private foodService:FoodService,public _authService: AuthService,
+    private cartService:CartService,private snackbar:MatSnackBar) { }
 
   ngOnInit(): void {
     let id=this.route.snapshot.params['id'];
@@ -21,4 +27,35 @@ export class FooddetailsComponent implements OnInit {
       console.log(this.food);
     })
   }
+
+  onCart(){
+    if(this._authService.getToken()){
+      this._authService.getUserId().subscribe((data)=>{
+        this.cart.userid=data;
+        this.cart.productid=this.food.id;
+        this.cartService.addCart(this.cart)
+          .subscribe(
+            res => {
+              this.snackbar.open('Added to Cart Successfully!!', 'OK', {
+                duration: 3000,
+              });
+            },
+            err => {
+              if( err instanceof HttpErrorResponse ) {
+                if (err.status === 401) {
+                  this.snackbar.open('Error Failed Adding to Cart', 'OK', {
+                    duration: 3000,
+                  });
+                }
+              }
+            });
+      })
+    }
+    else{
+      this.snackbar.open('Please Login To Add to Cart', 'OK', {
+        duration: 3000,
+      });
+    }
+  }
+
 }
--- mealmate/src/app/home/home.component.ts
+++ mealmate/src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food.service';
+import { AuthService } from '../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { KitchentoolsService } from '../services/kitchentools.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +14,33 @@ import { FoodService } from '../services/food.service';
 export class HomeComponent implements OnInit {
 
   food;
-  constructor(private foodService:FoodService){
-    
+  kitchentls;
+  UserId;
+  User;
+  constructor(private foodService:FoodService,public _authService: AuthService,private snackbar:MatSnackBar,private kitchentoolService:KitchentoolsService,private _router: Router){
+    if(this._authService.getToken()){
+      this._authService.getUserId().subscribe((data)=>{
+        this.UserId=data;
+        this._authService.getUserName(this.UserId).subscribe((name)=>{
+          this.User=name;
+        })
+      })
+    }
   }
 
   ngOnInit(): void {
     this.foodService.getMainFood().subscribe((data)=>{
       this.food=data;
+    })
+    this.kitchentoolService.getMainKitchenTools().subscribe((data:any[])=>{
+      this.kitchentls=data;
+    },
+    err => {
+      if( err instanceof HttpErrorResponse ) {
+        if (err.status === 401) {
+          this._router.navigate(['/login'])
+        }
+      }
     })
     // this.food=this.foodservice.getMainFood();
   }
