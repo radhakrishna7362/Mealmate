624c127456e5a8eabf8cecd7472feac323624425
radhakrishna7362 <7krishna3radha@gmail.com>
2020-11-19 12:34:32 +0530

    minor changes

--- mealmate/src/app/thank-you/thank-you.component.ts
+++ mealmate/src/app/thank-you/thank-you.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-thank-you',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThankYouComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+        this.router.navigate(['/orders']);
+    }, 2000);
   }
 
 }
